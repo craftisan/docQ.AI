62c38c465294209dc0af194627753dd707cc22f2
DJ <contact@deekshantjoshi.com>
2025-07-13 23:26:54 +0530

    [Update] Complete implementation - Env config and app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DocumentsModule } from './documents/documents.module';
+import { User } from './users/user.entity';
+import { Document } from './documents/document.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+      entities: [User, Document],
+    }),
+    AuthModule,
+    UsersModule,
+    DocumentsModule,
+  ],
 })
 export class AppModule {}
